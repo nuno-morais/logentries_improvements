f451f21792c6c1b9c748aedd569db2499f482319
Nuno Morais <nram@nuno-morais.eu>
2019-04-04 11:55:37 +0100

    Add new events

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function getConsumerEventByName(name, event) {
 }
 
 function getEventByName(name, event) {
-  let reg = /^(.*?)event(=>)"(.*?)"/;
+  let reg = /^(.*?)event((=>)|:)"(.*?)"/;
   let newReg = new RegExp(
     reg
       .toString()
@@ -77,37 +77,40 @@ rl.on('line', line => {
   if (line != '' && line != '/n') {
     const date = getDate(line);
     const time = transformeDateIntoInteger(date);
-    const consuming_event_name = getConsumerEventByName('event', line);
-    const event_correlation = getConsumerEventByName('correlation_id', line) || getEventByName('correlation_id', line);
+    const consuming_event_name = getConsumerEventByName('event', line) || getEventByName('verb', line);
+    const event_correlation =
+      getConsumerEventByName('correlation_id', line) ||
+      getEventByName('correlation_id', line) ||
+      getEventByName(':type=>"step", :id', line);
     const emitting_event_name = getPublishEventByNameOrchestrator('event_name', line);
     const key = getRoutingKey(line) || getPublishEventByNameOrchestrator('resource', line);
 
-    if (consuming_event_name || emitting_event_name) {
-      events.push({
-        date: date,
-        time: time,
-        event_direction: consuming_event_name ? 'Consumed' : 'Emitted',
-        key: key,
-        event_correlation: event_correlation,
-        event_name: consuming_event_name || emitting_event_name,
-        event_content: line
-      });
-    }
+    events.push({
+      date: date,
+      time: time,
+      event_direction: consuming_event_name ? 'Consumed' : 'Emitted',
+      key: key,
+      event_correlation: event_correlation,
+      event_name: consuming_event_name || emitting_event_name,
+      event_content: line
+    });
   }
 });
 
 rl.on('close', () => {
   events.sort((a, b) => a.time - b.time);
   console.table(
-    events.map(e => {
-      return {
-        'Event Date': e.date,
-        Direction: e.event_direction,
-        'Event Name': e.event_name,
-        Key: e.key,
-        'Correlation Id': e.event_correlation
-      };
-    })
+    events
+      .filter(q => q.event_name != null && q.event_name != 'initialize' && q.event_name != 'execute')
+      .map(e => {
+        return {
+          'Event Date': e.date,
+          Direction: e.event_direction,
+          'Event Name': e.event_name,
+          Key: e.key,
+          'Correlation Id': e.event_correlation
+        };
+      })
   );
   writeStream.write(
     events.map(e => e.date + ' ' + e.event_name + ' ' + e.event_correlation + ' <> ' + e.event_content).join('\n')
